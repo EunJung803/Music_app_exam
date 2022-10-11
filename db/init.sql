95a389c57395a3c2b5f5a76715810aff77a8429c
EunJung803 <aimee3322@gmail.com>
2022-10-11 23:49:59 +0900

    Feat : 유틸, initData 기반, JPA 기반, AppConfg 기반

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS music_app__test;
+CREATE DATABASE music_app__test;
+USE music_app__test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS music_app__dev;
+CREATE DATABASE music_app__dev;
+USE music_app__dev;
---
+++ src/main/java/com/ll/exam/music_app/app/base/initData/InitDataBefore.java
@@ -0,0 +1,7 @@
+package com.ll.exam.music_app.app.base.initData;
+
+public interface InitDataBefore {
+    default void before() {
+
+    }
+}
